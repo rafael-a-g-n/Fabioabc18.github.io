4183904c62c446ad2dfa171338e4614d34a10db8
Rwedoll <ragn.ryzen@gmail.com>
2024-04-11 20:17:28 +0100

    Update build_files.sh to set up Python virtual environment and install requirements

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-pip install -r requirements.txt
+#!/bin/bash
+
+# Set up Python virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install requirements and collect static files
+pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
